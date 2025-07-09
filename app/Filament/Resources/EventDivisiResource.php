9e2c90731fcc17833de648aaf0a3fd7ae6439d10
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-07-09 19:15:55 +0700

    efisiensi

--- app/Filament/Resources/EventDivisiResource.php
+++ app/Filament/Resources/EventDivisiResource.php
@@ -61,15 +61,15 @@ class EventDivisiResource extends Resource
                         TextInput::make('judul')
                             ->label('Judul')
                             ->required(),
-                        FileUpload::make('foto')
-                            ->label('Foto')
-                            ->maxSize(1000)
-                            ->maxFiles(3)
-                            ->directory('uploads/foto')
-                            ->image()
-                            ->multiple()
-                            ->reorderable()
-                            ->required(),
+                        // FileUpload::make('foto')
+                        //     ->label('Foto')
+                        //     ->maxSize(1000)
+                        //     ->maxFiles(3)
+                        //     ->directory('uploads/foto')
+                        //     ->image()
+                        //     ->multiple()
+                        //     ->reorderable()
+                        //     ->required(),
                         DatePicker::make('tanggal_upload')
                             ->label('Tanggal Upload')
                             ->required(),
@@ -155,7 +155,6 @@ class EventDivisiResource extends Resource
                                 'Tidak Aktif' => 'danger',
                                 default => 'gray',
                             }),
-
                     ])->space(1),
                 ])->space(3),
             ])
