227f3b2aca5d879be5916fd3407b8d711d39589d
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-07-10 14:45:24 +0700

    efisiensi

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ Route::get('/{any}', function () {
     return Inertia::render('Index');
 })->where('any', '.*');
 
+Route::get('/memory-limit', function () {
+    return ini_get('memory_limit');
+});
+
 // //Article
 // Route::get('/article', [ArtikelController::class, 'index'])->name('article.index');
 // Route::get('/article/latest', [ArtikelController::class, 'latest'])->name('article.latest');
