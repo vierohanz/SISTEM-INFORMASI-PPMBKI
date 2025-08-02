37f8017dfb45c3e90e4c36c903031e146d3d59f2
Rais Hannan Rizanto <114925063+vierohanz@users.noreply.github.com>
2025-08-02 12:56:12 +0700

    psb skipped

--- resources/js/Pages/Navbar.jsx
+++ resources/js/Pages/Navbar.jsx
@@ -89,10 +89,10 @@ export default function Navbar() {
                                         </li>
                                         <li>
                                             <Link
-                                                to="/psb"
+                                                // to="/psb"
                                                 className="block px-4 py-2 hover:bg-emerald-100 text-[11px] text-gray-500"
                                             >
-                                                PSB
+                                                PSB (Maintenance)
                                             </Link>
                                         </li>
                                     </motion.ul>
